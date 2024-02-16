b50660fba70ca9b75a559fb9a975f056a731d1a0
Salamiqoyum <160117557+Salamiqoyum@users.noreply.github.com>
2024-02-16 10:00:35 +0100

    Add files via upload

---
+++ Salami.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONIYITAN_TOBILOBA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "polyilaro";
+
+            // Declare second integer, double,and string varibale
+
+            int a = 12;
+            double b = 4.0;
+            string c = "is the best place to learn computer science";
+           
+            // printing of the variable declared 
+
+            Console.WriteLine(i + a);
+            Console.WriteLine(d + b);
+            Console.WriteLine(s + " "+ c);
+
+
+
+
+        }
+    }
+}
+
